de38298f0cf7be54da32d78694bc4b76ce0ca0df
Alexey Andreev <bezumny.tochk@gmail.com>
2017-01-31 16:36:11 +0400

    Added parser and db importer for rkn

--- main.go
+++ main.go
@@ -5,15 +5,42 @@ import (
 	"os"
 	"log"
 	"strings"
-	//"github.com/jmoiron/sqlx"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/go-sql-driver/mysql"
+	"flag"
+	"io/ioutil"
+	"encoding/json"
+	"fmt"
 )
 
 type urlElement struct {
-	url string
-	reg *string
+	url string `db:"url"`
+	reg *string `db:"reg"`
+}
+
+var (
+	configFile = flag.String("Config", "conf.json", "Where to read the Config from")
+)
+
+var config struct {
+	MysqlLogin    string `json:"mysqlLogin"`
+	MysqlPassword string `json:"mysqlPassword"`
+	MysqlHost     string `json:"mysqlHost"`
+	MysqlDb       string `json:"mysqlDb"`
+}
+
+func loadConfig(path string) error {
+	jsonData, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(jsonData, &config)
 }
 
 func main() {
+	flag.Parse()
+	loadConfig(*configFile)
+
 	file, err := os.Open("rkn")
 	if err != nil {
 		log.Fatal(err)
@@ -33,6 +60,25 @@ func main() {
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
+	}
+
+	dataBase, err := sqlx.Connect("mysql", config.MysqlLogin+":"+config.MysqlPassword+"@tcp("+config.MysqlHost+")/"+config.MysqlDb+"?charset=utf8")
+	defer dataBase.Close()
+	if err != nil {
+		log.Print(err)
+	}
+	for _, singleUrl := range urls {
+		fmt.Println(singleUrl.url)
+		if singleUrl.reg != nil {
+			*singleUrl.reg = "(" + *singleUrl.reg
+			_, err = dataBase.NamedQuery("INSERT INTO `urls` (`url`, `reg`) VALUES (:url, :reg)", map[string]interface{}{ "url": singleUrl.url, "reg": *singleUrl.reg,})
+		} else {
+			_, err = dataBase.NamedQuery("INSERT INTO `urls` (`url`, `reg`) VALUES (:url, :reg)", map[string]interface{}{ "url": singleUrl.url, "reg": singleUrl.reg,})
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 }
